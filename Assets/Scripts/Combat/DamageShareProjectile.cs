ff4b76ab37d5fa9a3974cde6a073e2a4996e3a9d
Liutauras Gaidamavičius <liutauras@unity3d.com>
2022-01-29 23:15:52 +0200

    Prefab updates

--- Assets/Scripts/Combat/DamageShareProjectile.cs
+++ Assets/Scripts/Combat/DamageShareProjectile.cs
@@ -7,14 +7,31 @@ public class DamageShareProjectile : MonoBehaviour
     public Health target;
     public int damageValue;
     public int speed;
+    public float randomDirStrengthStart = 3f;
+    public float randomDirDecreaseSpeed = 1f;
+
+    Vector3 randomDir;
+    float randomDirStrength;
 
 
-    // Update is called once per frame
+    void Start()
+    {
+        randomDir = Random.onUnitSphere;
+        randomDirStrength = randomDirStrengthStart;
+    }
     void Update()
     {
+        if(!target) Destroy(gameObject);
+
         Vector3 dirToTarget = target.transform.position - transform.position;
         dirToTarget = dirToTarget.normalized;
         transform.Translate(dirToTarget * speed * Time.deltaTime);
+
+        if(randomDirStrength > 0f)
+        {
+            transform.Translate(randomDir * randomDirStrength * Time.deltaTime);
+            randomDirStrength -= randomDirDecreaseSpeed * Time.deltaTime;
+        }
     }
 
     void OnTriggerEnter(Collider other)
